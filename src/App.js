f7b2cd8850779166e3d8deb690892a583d68afbf
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-09 11:57:37 +0530

    use of async and await

--- src/App.js
+++ src/App.js
@@ -7,20 +7,19 @@ function App() {
 
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/").then(response => {
-      return response.json();
-    }).then(data => {
-      const transformedMovies = data.results.map(movieData => {
+  async function fetchMoviesHandler() {
+    
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();    
+    const transformedMovies = data.results.map(movieData => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.opening_crawl,
           releaseDate: movieData.release_date          
         }
-      });
-      return setMovies(transformedMovies);
-    })
+    });
+    setMovies(transformedMovies);
   };
 
   // console.log(movies);
